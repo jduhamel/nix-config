dbc5d1a9713bebac460b76c1750c435373e988f4
jduhamel <jduhamel@gmail.com>
2022-05-27 08:25:55 -0400

    Add(cachix) my cachix

---
+++ system/cachix/jduhamel-nixos.nix
@@ -0,0 +1,11 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://jduhamel-nixos.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "jduhamel-nixos.cachix.org-1:jwXnlnlOileKAFwgn/jddIc43hyy+D32nFtodwvbsyU="
+    ];
+  };
+}
